cc4a654fc03b3cf4b1222625167fa7bae88ceca7
Logan John Owens <loganjohnowens@users.noreply.github.com>
2024-11-12 21:26:54 -0700

    added text

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ def gravity():
 def ground(ground_pos, size):
     global pos
     global on_ground
-    on_ground = False
+    if on_ground is not True:
+        on_ground = False
     if pos[0] > ground_pos[0] and pos[0] < ground_pos[0] + size[0] and pos[1] == ground_pos[1] - 121:
         pos[1] -= 1
         on_ground = True
@@ -59,15 +60,25 @@ def ground(ground_pos, size):
     while number < size[0] - 18:
         number += 18
         screen.blit(images[5], (ground_pos[0] + number, ground_pos[1]))
-    screen.blit(images[4], (ground_pos[0] + number, ground_pos[1]))
+    screen.blit(images[4], (ground_pos[0] + number, ground_pos[1] - 2))
 
 
 def level():
     global level_number
+    global sub_level_number
+    global on_ground
+    on_ground = False
     if level_number == 1:
         backround(9)
-        screen.blit(images[10], (310, 150))
-        ground([-1, 200,], [300, 10])
+        if sub_level_number == 1:
+            screen.blit(images[10], (310, 150))
+            ground([-1, 200,], [300, 10])
+            ground([300, 340,], [480, 10])
+        if sub_level_number == 2:
+            screen.blit(images[11], (310, 50))
+            text_maker(['I want to go out side.', 'one day', ''])
+            ground([-1, 200], [150, 0])
+            ground([149, 450], [650, 0])
 
 
 def jumping():
@@ -99,7 +110,31 @@ def update_and_time():
     time.sleep(.001)
 
 
+def level_updating():
+    global pos
+    global sub_level_number
+    global sub_level_spawn
+    if pos[0] > 799:
+        sub_level_number += 1
+        pos = sub_level_spawn[sub_level_number - 1]
+
+
+def text_maker(lines):
+    global pos
+    screen.blit(images[12], (pos[0], pos[1] - 50))
+    font = pygame.font.Font(None, 15)
+    line_height = font.get_height() + 5  # Add a bit of spacing between lines
+
+    for index, words in enumerate(lines):
+        text_surface = font.render(words, True, (0, 0, 0))
+        text_rect = text_surface.get_rect(
+            center=(pos[0] + 60, pos[1] - 20 + index * line_height))
+        screen.blit(text_surface, text_rect)
+
+
 level_number = 1
+sub_level_number = 2
+sub_level_spawn = [[-1, 0], [-1, 50]]
 image = 0
 pos = [0, 0]
 jump = False
@@ -115,4 +150,5 @@ while True:
     movemnt()
     jumping()
     gravity()
+    level_updating()
     update_and_time()
--- pictures.py
+++ pictures.py
@@ -4,26 +4,50 @@ image = []
 
 
 def import_sprites():
-    image.append(pygame.image.load(os.path.join("images", "main_guy_standing.png")).convert_alpha())
+    image.append(pygame.image.load(os.path.join(
+        "images", "main_guy_standing.png")).convert_alpha())
     image[0] = pygame.transform.scale(image[0], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "main_guy_walking_right.png")).convert_alpha())
+    image.append(pygame.image.load(os.path.join(
+        "images", "main_guy_walking_right.png")).convert_alpha())
     image[1] = pygame.transform.scale(image[1], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "main_guy_walking_left.png")).convert_alpha())
+    image.append(pygame.image.load(os.path.join(
+        "images", "main_guy_walking_left.png")).convert_alpha())
     image[2] = pygame.transform.scale(image[2], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "left_side_of_ground.png")).convert_alpha())
+    image.append(pygame.image.load(os.path.join(
+        "images", "left_side_of_ground.png")).convert_alpha())
     image[3] = pygame.transform.scale(image[3], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "right_side_of_ground.png")).convert_alpha())
+    image[3] = image[3].subsurface(pygame.Rect(0, 0, 20, 20)).copy()
+    image.append(pygame.image.load(os.path.join(
+        "images", "right_side_of_ground.png")).convert_alpha())
     image[4] = pygame.transform.scale(image[4], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "middle_ground.png")).convert_alpha())
+    image[4] = image[4].subsurface(pygame.Rect(0, 0, 38, 30)).copy()
+    image.append(pygame.image.load(os.path.join(
+        "images", "middle_ground.png")).convert_alpha())
     image[5] = pygame.transform.scale(image[5], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "main_guy_crouching.png")).convert_alpha())
+    image[5] = image[5].subsurface(pygame.Rect(0, 0, 20, 20)).copy()
+    image.append(pygame.image.load(os.path.join(
+        "images", "main_guy_crouching.png")).convert_alpha())
     image[6] = pygame.transform.scale(image[6], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "main_guy_crouching_and_walking_left.png")).convert_alpha())
+    image[6] = image[6].subsurface(pygame.Rect(0, 0, 50, 100)).copy()
+    image.append(pygame.image.load(os.path.join(
+        "images", "main_guy_crouching_and_walking_left.png")).convert_alpha())
     image[7] = pygame.transform.scale(image[7], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "main_guy_crouching_and_walking_right.png")).convert_alpha())
+    image[7] = image[7].subsurface(pygame.Rect(0, 0, 50, 100)).copy()
+    image.append(pygame.image.load(os.path.join(
+        "images", "main_guy_crouching_and_walking_right.png")).convert_alpha())
     image[8] = pygame.transform.scale(image[8], (200, 200))
-    image.append(pygame.image.load(os.path.join("images", "backround.png")).convert_alpha())
+    image[8] = image[8].subsurface(pygame.Rect(0, 0, 50, 100)).copy()
+    image.append(pygame.image.load(os.path.join(
+        "images", "backround.png")).convert_alpha())
     image[9] = pygame.transform.scale(image[9], (800, 600))
-    image.append(pygame.image.load(os.path.join("images", "window.png")).convert_alpha())
+    image.append(pygame.image.load(os.path.join(
+        "images", "window.png")).convert_alpha())
     image[10] = pygame.transform.scale(image[10], (800, 600))
+    image.append(pygame.image.load(os.path.join(
+        "images", "picture_1.png")).convert_alpha())
+    image[11] = pygame.transform.scale(image[11], (200, 200))
+    image.append(pygame.image.load(os.path.join(
+        "images", "speech_bubble.png")).convert_alpha())
+    image[12] = pygame.transform.scale(image[12], (450, 500))
+    image[12] = image[12].subsurface(pygame.Rect(0, 0, 200, 100)).copy()
     return image
